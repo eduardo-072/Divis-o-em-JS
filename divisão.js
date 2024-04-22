955fef06313fb8d4159cdfa32b9f1a71097e248d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-22 19:46:49 -0300

    feito

---
+++ divisão.js
@@ -0,0 +1,6 @@
+let numerador = parseInt(prompt("Digite um número"));
+let denominador = parseInt(prompt("Digite um número"));
+
+let resultado = numerador / denominador;
+alert(numerador + " dividido por " + denominador + " é "  + (resultado)) 
+// alert("Seu numerador é " + (numerador) + "e seu denominador é " + (denominador) +   "e o resultado foi de");
