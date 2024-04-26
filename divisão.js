d17eb00aed67dc0c767492ab57c9d6c73ea0ec07
eduardo-072 <eduardosilvaaa072@gmail.com>
2024-04-25 23:55:16 -0300

    refeito

--- divisão.js
+++ divisão.js
@@ -3,4 +3,3 @@ let denominador = parseInt(prompt("Digite um número"));
 
 let resultado = numerador / denominador;
 alert(numerador + " dividido por " + denominador + " é "  + (resultado)) 
-// alert("Seu numerador é " + (numerador) + "e seu denominador é " + (denominador) +   "e o resultado foi de");
